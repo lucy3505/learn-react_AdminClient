1b2737390c4b85a2b33350f41e83e5e594c95d5c
xuyanyu <13022112163@163.com>
2019-12-23 14:13:58 +0800

    change git

--- 3.git配置.js
+++ 3.git配置.js
@@ -56,11 +56,13 @@
 */
 
 /*
-# 设置ss git config --global http.proxy 'socks5://127.0.0.1:1080'
+# 设置ss
+git config --global http.proxy 'socks5://127.0.0.1:1080'
+! git config --global http.proxy 'http://127.0.0.1:1080'
 
 git config --global https.proxy 'socks5://127.0.0.1:1080'
 
-!设置代理 git config --global https.proxy http://127.0.0.1:1080
+设置代理 git config --global https.proxy http://127.0.0.1:1080
 
 git config --global https.proxy https://127.0.0.1:1080
 
