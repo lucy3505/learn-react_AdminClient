948b40bc98edab0a27ff6bfae921e91fb31ff613
xuyanyu <13022112163@163.com>
2019-12-23 14:11:06 +0800

    change git

--- 3.git配置.js
+++ 3.git配置.js
@@ -54,3 +54,19 @@
     这时候你就进了新分支
 
 */
+
+/*
+# 设置ss git config --global http.proxy 'socks5://127.0.0.1:1080'
+
+git config --global https.proxy 'socks5://127.0.0.1:1080'
+
+!设置代理 git config --global https.proxy http://127.0.0.1:1080
+
+git config --global https.proxy https://127.0.0.1:1080
+
+!取消代理 git config --global --unset http.proxy
+
+git config --global --unset https.proxy
+
+
+*/
